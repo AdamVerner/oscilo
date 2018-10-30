0226c37751103a8e4a277c56c93bfa797a2ff162
Adam Verner <adam.verner@mensa.cz>
2018-10-30 23:15:25 +0100

    init main

---
+++ Software/MainWindow.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env/ python3
+# -*- coding: utf-8 -*-
+"""
+"""
+
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+
+class MainWindow(Gtk.Window):
+    """
+
+    """
+
+    def __init__(self):
+        Gtk.Window.__init__(self)
+
+        self.set_title('Oscilo')
+        self.set_default_size(1920, 600)
+        self.set_border_width(50)
+        self.connect('destroy', Gtk.main_quit)
+
+        self.mainGrid = Gtk.Grid()
+        self.add(self.mainGrid)
+
+        self.show_all()
+
+
+if __name__ == '__main__':
+    main = MainWindow()
+    Gtk.main()
