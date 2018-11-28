622d6221c9aa5e453b3c00c06c032bc7857f4d8c
averner <adam.verner@mensa.cz>
2018-11-28 12:32:42 +0100

    sampler++

--- firmware/sampler.sv
+++ firmware/sampler.sv
@@ -1,8 +1,10 @@
 /*
 *   statemodule
 *   the hearth of the whole osciloscope
-*
-*/
+*   TODO shift the memory, so the first sample is on the first place in
+*   memmory
+*   TODO more advanced trigger logic (external module)
+*/ 
 
 module sampler #(
     parameter module_id = 8'h10,
@@ -22,16 +24,30 @@ module sampler #(
     output              m_addr,
     output              m_we, /* write-enable */
     output              m_cs /* chip-select */
+
+    output              m_offset /*  */
     );
 
 
-    reg [SAMPLE_DEPTH<<1/2:0] post_samples;  /* 50% trigger placement */
+    reg [SAMPLE_DEPTH<<2:0]   post_samples;  /* 50% trigger placement */
     reg [SAMPLE_DEPTH<<1:0]   offset;
 
-    reg                    trig;
+    reg                       active
+    reg                       trig;
 
-    typedef enum logic[1:0] {
+    /****** module state watcher ******/
+    always @(posedge state_change)
+    begin
+        if(state == module_id)
+            active <= 1;
+        else
+            active <= 1;
+
+    end
+
+    typedef enum logic[2:0] {
         IDLE,
+        SETUP,
         PRE_TRIG,
         POST_TRIG,
         CLEAN
@@ -40,38 +56,50 @@ module sampler #(
 
     assign clk_adc = clk;
 
+    /****** FSM `*****/
     always @(posedge clk_adc)
-        case(sampler)
-            IDLE: begin /* init */
-                m_addr <= 0;
-                trig <= 0;
-                post_samples <= 0;
+        case(sampler_state)
+            IDLE: begin
                 if(active)
-                    faze = 1;
+                    sampler_state = SETUP;
             end
-            PRE_TRIG: begin /* pre-tring */
+            SETUP: begin
+                m_we    <= 1;
+                m_cs    <= 1;
+                m_addr <= 1;
+                post_samples <= 0;
+                sample_state <= PRE_TRIG
+            end
+            /* store samples in memmory and add 1 to address and let it
+            * overflow indefinitley (redneck circular-buffer) */
+            PRE_TRIG: begin /* pre-tringer */
                 mem[m_addr] <= data_in;
-                m_addr <= m_addr+ 1;
+                m_addr      <= m_addr+ 1;
                 if(trig)
                 begin
-                    faze <= 2'b10;
-                    offset <= m_addr;;
+                    sampler_state <= POST_TRIG
+                    m_offset <= m_addr;;
                 end
             end
+            /* continue to store samples, but also watch for the number of
+            * amples, if its more than half of of the buffer, goto next */
             POST_TRIG: begin /* post-trig */
                 mem[m_addr] <= data_in;
-                m_addr <= m_addr+ 1;
+                m_addr <= m_addr + 1;
                 post_samples <= post_samples + 1;
                 if(&post_samples)  /* reduction AND */
-                    faze <= 2'b11;
+                    sampler_state <= CLEAN;
             end
             CLEAN: begin
+                m_we = 0;
+                m_cs = 0;
+                sampler_state <= IDLE;
             end
         endcase
 
 
     /***** TRIGGER LOGIC *****/
-    reg thr1, thr2, trig;
+    reg thr1, thr2;
     always @(posedge clk_adc)
     begin
         thr1 <= (data_in >= 8'h80);
@@ -80,9 +108,5 @@ module sampler #(
 
     /* assert true for one clock at rising edge */
     assign trig = thr1 & ~thr2;
-
-
-
-
 
 endmodule
