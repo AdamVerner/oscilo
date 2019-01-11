38f081950ea3f618afadcb6ff9a29ee06a199b78
Adam Verner <averner@ntb>
2019-01-11 10:09:32 +0100

    add fake adc module that generates ramp

---
+++ firmware/fake_adc.sv
@@ -0,0 +1,24 @@
+/*
+ * generates ramping signal
+ */
+
+module fake_adc(
+				input         clk,
+				input         rst,
+				output [11:0] data_out,
+				);
+
+initial begin
+data_out = 0;
+
+
+always @(posedge clk)
+begin
+	if(~rst)
+		data_out = 0;
+	else
+		data_out += 1;
+
+end
+
+endmodule
