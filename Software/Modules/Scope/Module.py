0a0e87843996ea2562ace92e3e8ae896ff323b57
Adam Verner <adam.verner@mensa.cz>
2019-03-11 03:21:46 +0100

    comment out unsupported signals module part

--- Software/Modules/Scope/Module.py
+++ Software/Modules/Scope/Module.py
@@ -38,8 +38,8 @@ class Module(Gtk.Grid):
         self.settings.control.push_func = self.plot.update
 
         self.attach(self.plot, 0, 0, 1, 1)
-        self.attach(self.signals, 1, 0, 1, 1)
-        self.attach(self.settings, 0, 1, 2, 1)
+        # self.attach(self.signals, 1, 0, 1, 1)
+        self.attach(self.settings, 0, 1, 1, 1)
 
     def redraw(self):
         self.plot.redraw()
